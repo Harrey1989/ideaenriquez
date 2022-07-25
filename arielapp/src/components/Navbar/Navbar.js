b3211f44408f51319376bcbed82167619839cd02
Ariel Enriquez <arielenriquez89@gmail.com>
2022-07-24 23:22:37 -0400

    Tercera entrega

--- arielapp/src/components/Navbar/Navbar.js
+++ arielapp/src/components/Navbar/Navbar.js
@@ -1,5 +1,79 @@
-function NavBar (){
-    return <div>
-        <h2>Navbar Page</h2>
-    </div>
-}
+import React, { useState } from "react";
+import {
+  Container,
+  LogoContainer,
+  Wrapper,
+  Menu,
+  MenuItem,
+  MenuItemLink,
+  MobileIcon,
+} from "./Navbar.elements";
+import {
+  FaBattleNet,
+  FaBars,
+  FaTimes,
+  FaHome,
+  FaUserAlt,
+  FaBriefcase,
+  FaGlasses,
+} from "react-icons/fa";
+import { IconContext } from "react-icons";
+
+const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  return (
+    <Container>
+      <Wrapper>
+        <IconContext.Provider value={{ style: { fontSize: "3em" } }}>
+          <LogoContainer>
+            <FaBattleNet />
+            <p>Ariel App - </p>
+            <p> Coderhouse</p>
+          </LogoContainer>
+
+          <MobileIcon onClick={() => setShowMobileMenu(!showMobileMenu)}>
+            {showMobileMenu ? <FaTimes /> : <FaBars />}
+          </MobileIcon>
+
+          <Menu open={showMobileMenu}>
+            <MenuItem>
+              <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                <div>
+                  <FaHome />
+                  HOME
+                </div>
+              </MenuItemLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                <div>
+                  <FaUserAlt />
+                  ABOUT ME
+                </div>
+              </MenuItemLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                <div>
+                  <FaBriefcase />
+                  PORTFOLIO
+                </div>
+              </MenuItemLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuItemLink onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                <div>
+                  <FaGlasses />
+                  CONTACT ME
+                </div>
+              </MenuItemLink>
+            </MenuItem>
+          </Menu>
+        </IconContext.Provider>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
